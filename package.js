a58af70a4f0b90032759bbf14592482be55ba264
Dean Radcliffe <deanmisc@yahoo.com>
2015-10-20 10:10:17 -0500

    satisfy the linter

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "deanius:dependito",
   summary: "Visualize your application's dependency structure",
   version: "0.0.1",
-  git: "https://github.com/deanius/meteor-dependito"
+  git: "https://github.com/deanius/meteor-dependito.git"
 });
 
 Package.onUse(function(api) {
